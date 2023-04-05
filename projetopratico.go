89089fe5fc9c7cb79e3f878f2569a2627824d1bc
MarcosDiorio <marcosdiorio@gmail.com>
2023-04-05 20:55:55 -0300

    criação do .exe

--- projetopratico.go
+++ projetopratico.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+)
+
+const ebuK = 373.0
+
 func main() {
+	tempK := ebuK
+	tempC := (tempK - 273.0)
+	fmt.Println("A temperatura de ebulição em Celsius é: ", tempC, "°C")
 
 }
